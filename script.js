8c9e98af24b5d25dc221f55083c0c389602a211c
jwt2706 <jwt2home@gmail.com>
2022-10-12 16:34:33 -0400

    bonework for menu bar

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-var a = 1;
+$(document).on("scroll", function() {
+    var pageTopVal = $(document).scrollTop();
+    var pageBottomVal = pageTopVal + $(window).height();
+    var logos = $(".logo");
+  
+    for (var i = 0; i < logos.length; i++) {
+  
+      if ($(logos[i]).position().top < pageBottomVal) {
+        $(logos[i]).addClass("visible");
+      } else {
+        $(logos[i]).removeClass("visible");
+      }
+    }
+  });
