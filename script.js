b6cafce6b5544ff2d32c7987b36bf2b786db82fa
jwt2706 <jwt2home@gmail.com>
2022-10-17 11:36:09 -0400

    opt. on calculations

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 window.addEventListener('scroll', function(){
-  var top = window.scrollY;
-  var maxTop = window.scrollMaxY;
-  var maxHeight = parseInt((getComputedStyle(document.querySelector(".menuBar"))).height);//fetches the height of le menu bar and rounds it
-  updateThermoLiquid(top, maxTop, maxHeight);
+  var top = window.scrollY; //fetches the user's scroll position
+  var maxTop = window.scrollMaxY; //fetches the maximun scroll position
+  var maxHeight = parseInt((getComputedStyle(document.querySelector(".menuBar"))).height);//fetches the height of the menu bar and rounds it
+  updateThermoLiquid(top, maxTop, maxHeight); //updates the menu's liquid status
 });
   
 function updateThermoLiquid(top, maxTop, height) {
-  var perc = (top*height)/maxTop;
-  perc = ((perc*72)/height)+"%";
-  document.getElementById("thermoLiquid").style.height = perc;
+  var perc = (top*height)/maxTop; //finds the value of the scroll position on the maximum height
+  perc = ((perc*72)/height)+"%"; //finds the percentage of that value on 72 (just below the side of the menu container which is 75%)
+  document.getElementById("thermoLiquid").style.height = perc; //updates the liquid's status
 }
