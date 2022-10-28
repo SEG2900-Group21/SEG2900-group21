6accb01c0480286696aa2c03e9d236e20e70a30f
jwt2706 <jwt2home@gmail.com>
2022-10-28 16:49:51 -0400

    style_guidelines

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ window.addEventListener('scroll', function(){
   var top = window.scrollY; //fetches the user's scroll position
   var maxTop = window.scrollMaxY; //fetches the maximun scroll position
   var maxHeight = parseInt((getComputedStyle(document.querySelector(".menuBar"))).height);//fetches the height of the menu bar and rounds it
-  updateThermoLiquid(top, maxTop, maxHeight); //updates the menu's liquid status
+  var offset = maxHeight*0.28;
+  updateThermoLiquid(top, maxTop, maxHeight, offset); //updates the menu's liquid status
 });
   
-function updateThermoLiquid(top, maxTop, height) {
+function updateThermoLiquid(top, maxTop, height, offset) {
   var perc = (top*height)/maxTop; //finds the value of the scroll position on the maximum height
-  perc = ((perc*72)/height); //finds the percentage of that value on 72 (just below the side of the menu container which is 75%)
+  perc -= offset;
+  perc = ((perc*100)/height); //finds the percentage of that value on 72 (just below the side of the menu container which is 75%)
   document.getElementById("thermoLiquid").style.height = perc+"%"; //updates the liquid's status
-  celsius = (perc*50)/72; //calculating the percentage based on the degree range offered by the product
-  document.getElementById("tempature").innerHTML = (parseInt(celsius+10))+"°C"; //updates tempature
+  celsius = (perc*50)/100; //calculating the percentage based on the degree range offered by the product
+  document.getElementById("tempature").innerHTML = (parseInt(celsius+24))+"°C"; //updates tempature
 }
 
 
